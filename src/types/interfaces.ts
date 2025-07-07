5b736db65c5c7e726cb4ac44ad1911a3fc8452c7
modrisco89 <geralt.odriscoll@gmail.com>
2025-07-07 19:53:07 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -18,9 +18,10 @@ export interface BaseMovieProps {
     genre_ids?: number[];
   }
   
- export interface BaseMovieListProps { 
-    movies: BaseMovieProps[];
-  }   
+export interface BaseMovieListProps {
+  movies: BaseMovieProps[];
+  selectFavourite: (movieId: number) => void;  //add this
+} 
 
     export interface MovieDetailsProps extends BaseMovieProps {
     genres: {
