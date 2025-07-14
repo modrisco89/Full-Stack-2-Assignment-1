0184ac88b3a589462ed58be4dc63ab6e09177cf1
modrisco89 <geralt.odriscoll@gmail.com>
2025-07-14 05:07:57 +0100

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
       </Routes>
     </BrowserRouter>
   );
