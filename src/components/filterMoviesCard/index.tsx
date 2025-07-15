d28cd505a170fa8a35acef13334447e627b0d0bc
modrisco89 <geralt.odriscoll@gmail.com>
2025-07-15 21:27:09 +0100

    Using react-query to manage the browser-based cache.

--- src/components/filterMoviesCard/index.tsx
+++ src/components/filterMoviesCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -9,9 +9,11 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import SortIcon from '@mui/icons-material/Sort';
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { FilterOption } from "../../types/interfaces"
+import { FilterOption, GenreData} from "../../types/interfaces"
 import { SelectChangeEvent } from "@mui/material";
 import { getGenres } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner';
 
 const styles = {
   root: {
@@ -33,20 +35,24 @@ interface FilterMoviesCardProps {
 }
 
 
-  const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({ titleFilter, genreFilter, onUserInput }) => {
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+ const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({ titleFilter, genreFilter, onUserInput }) => {
+  const { data, error, isLoading, isError } = useQuery<GenreData, Error>("genres", getGenres);
 
- useEffect(() => {
-    getGenres().then((allGenres) => {
-      setGenres([genres[0], ...allGenres]);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h1>{(error as Error).message}</h1>;
+  }
+  const genres = data?.genres || [];
+  if (genres[0].name !== "All") {
+    genres.unshift({ id: "0", name: "All" });
+  }
 
-      const handleChange = (e: SelectChangeEvent, type: FilterOption, value: string) => {
-        e.preventDefault()
-        onUserInput(type, value)
-      };
+  const handleChange = (e: SelectChangeEvent, type: FilterOption, value: string) => {
+    e.preventDefault()
+      onUserInput(type, value)
+  };
 
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
     handleChange(e, "title", e.target.value)
@@ -55,6 +61,7 @@ interface FilterMoviesCardProps {
   const handleGenreChange = (e: SelectChangeEvent) => {
     handleChange(e, "genre", e.target.value)
   };
+
   return (
     <>
     <Card sx={styles.root} variant="outlined">
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -58,3 +58,18 @@ export type FilterOption = "title" | "genre";
     content: string
     author: string
   }
+
+
+  export interface GenreData {
+  genres: {
+    id: string;
+    name: string
+  }[];
+}
+
+export interface DiscoverMovies {
+  page: number;	
+  total_pages: number;
+  total_results: number;
+  results: BaseMovieProps[];
+}
