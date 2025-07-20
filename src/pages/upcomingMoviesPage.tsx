d33977d0beeecc6bcc6e1142b127238711e2681a
modrisco89 <geralt.odriscoll@gmail.com>
2025-07-20 16:03:56 +0100

    Change upcoming movies page to use caching. (Note: Replaced UpcomingMoviesPage with code from homepage and made necessary adjustments

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -50,10 +50,6 @@ const upcomingMoviesPage: React.FC = () => {
   const movies = data ? data.results : [];
   const displayedMovies = filterFunction(movies);
 
-  // Redundant, but necessary to avoid app crashing.
-  // const favourites = movies.filter(m => m.favourite)
-  // localStorage.setItem("favourites", JSON.stringify(favourites));
-  // const addToFavourites = (movieId: number) => true;
 
  return (
     <>
@@ -73,42 +69,3 @@ const upcomingMoviesPage: React.FC = () => {
   );
 };
 export default upcomingMoviesPage;
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import PageTemplate from '../components/templateMovieListPage';
-// import { BaseMovieProps } from "../types/interfaces";
-// import { getUpcomingMovies } from "../api/tmdb-api";
-
-
-// const upcomingMoviesPage: React.FC = () => {
-//   // eslint-disable-next-line react-hooks/rules-of-hooks
-//   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
-//   const favourites = movies.filter(m => m.favourite)
-//   localStorage.setItem('favourites', JSON.stringify(favourites))
-//   // New function
-//   const addToFavourites = (movieId: number) => {
-//     const updatedMovies = movies.map((m: BaseMovieProps) =>
-//       m.id === movieId ? { ...m, favourite: true } : m
-//     );
-//     setMovies(updatedMovies);
-//   };
-
-//  useEffect(() => {
-//     getUpcomingMovies().then(movies => {
-//       setMovies(movies);
-//     });
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return (
-//     <PageTemplate
-//       title='Upcoming Movies'
-//       movies={movies}
-//       selectFavourite={addToFavourites}
-//     />
-//   );
-// };
-// export default c;
